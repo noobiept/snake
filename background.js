fc03ca91aff7916171d6c34fd9471afe02a30fd2
Pedro Ferreira <plferreira4@gmail.com>
2018-09-07 17:27:57 +0100

    Remove some un-necessary files.

--- background.js
+++
@@ -1,8 +0,0 @@
-/*global chrome*/
-
-chrome.app.runtime.onLaunched.addListener( function() {
-    chrome.app.window.create( 'index.html', {
-        'state': 'maximized',
-        'id': 'default'
-    });
-});
