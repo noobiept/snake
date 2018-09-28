c101540654e397a338162b03b13ed4d2c76aeaeb
Pedro Ferreira <plferreira4@gmail.com>
2018-09-28 17:15:22 +0100

    Update the release build script.
    - Remove the js/css minimizers.
    - Use the 'tsconfig.json' configuration when building the typescript code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,9 @@ module.exports = function ( grunt ) {
         // typescript
         ts: {
             release: {
-                src: [ root + 'scripts/*.ts' ],
-                dest: 'temp/code.js',
+                tsconfig: './tsconfig.json',
+                passThrough: true,
                 options: {
-                    target: 'es5',
                     sourceMap: false
                 }
             }
@@ -39,60 +38,23 @@ module.exports = function ( grunt ) {
                 expand: true,
                 cwd: root,
                 src: [
+                    'css/**',
                     'images/*.png',
                     'libraries/**',
-                    'background.js',
-                    'manifest.json'
+                    'scripts/*.js',
+                    'index.html'
                 ],
                 dest: dest
             }
         },
-
-        // minimize the javascript
-        uglify: {
-            release: {
-                files: [ {
-                    src: 'temp/code.js',
-                    dest: dest + 'min.js'
-                } ]
-            }
-        },
-
-        cssmin: {
-            release: {
-                files: [ {
-                    expand: true,
-                    cwd: root + 'css/',
-                    src: '*.css',
-                    dest: dest + 'css/'
-                } ]
-            },
-            options: {
-                advanced: false
-            }
-        },
-
-        processhtml: {
-            release: {
-                files: [ {
-                    expand: true,
-                    cwd: root,
-                    src: 'index.html',
-                    dest: dest
-                } ]
-            }
-        }
     } );
 
     // load the plugins
     grunt.loadNpmTasks( 'grunt-ts' );
     grunt.loadNpmTasks( 'grunt-contrib-copy' );
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
     grunt.loadNpmTasks( 'grunt-contrib-clean' );
-    grunt.loadNpmTasks( 'grunt-processhtml' );
 
     // tasks
     grunt.registerTask( 'dev', [ 'copy:dev' ] );
-    grunt.registerTask( 'build', [ 'clean', 'ts', 'copy:release', 'uglify', 'cssmin', 'processhtml' ] );
+    grunt.registerTask( 'build', [ 'clean', 'ts', 'copy:dev', 'copy:release' ] );
 };
