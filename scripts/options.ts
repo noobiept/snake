0668ffb292aba0e0ddb36089263c2e79c642af70
Pedro Ferreira <plferreira4@gmail.com>
2018-11-14 22:16:44 +0000

    Add some extra options to the game (initial commit).
    - We will be able to change the wall/food/etc values directly as an option.
    - Remove the 'Difficulty' option.
    - Have a single 'get()' and 'set()' function for the options.

--- scripts/options.ts
+++ scripts/options.ts
@@ -1,29 +1,33 @@
 import * as AppStorage from './app_storage.js';
-import { changeCanvasDimensions } from './main.js';
 
 
 interface OptionsData {
     columns: number;
     lines: number;
     frameOn: boolean;
-    difficulty: Difficulty;
+    wallInterval: number;   // the intervals are in milliseconds
+    foodInterval: number;
+    doubleFoodInterval: number;
+    snakeSpeed: number;
 }
 
-export enum Difficulty {
-    normal = 0,
-    hard = 1
-};
-
-var OPTIONS: OptionsData = {
+const OPTIONS: OptionsData = {
     columns: 50,
     lines: 50,
     frameOn: false,
-    difficulty: Difficulty.normal
+    wallInterval: 4000,
+    foodInterval: 1000,
+    doubleFoodInterval: 5000,
+    snakeSpeed: 50
 };
 
 
-export function load( options: OptionsData ) {
+/**
+ * Validate and load the given options (if not valid then it uses the default values).
+ */
+export function load( options?: OptionsData ) {
     if ( options ) {
+
         if ( Number.isInteger( options.columns ) ) {
             OPTIONS.columns = options.columns;
         }
@@ -32,66 +36,48 @@ export function load( options: OptionsData ) {
             OPTIONS.lines = options.lines;
         }
 
-        if ( typeof options.frameOn !== 'undefined' ) {
+        if ( typeof options.frameOn === 'boolean' ) {
             OPTIONS.frameOn = options.frameOn;
         }
 
-        if ( typeof options.difficulty !== 'undefined' ) {
-            OPTIONS.difficulty = options.difficulty;
+        if ( Number.isInteger( options.wallInterval ) ) {
+            OPTIONS.wallInterval = options.wallInterval;
+        }
+
+        if ( Number.isInteger( options.foodInterval ) ) {
+            OPTIONS.foodInterval = options.foodInterval;
+        }
+
+        if ( Number.isInteger( options.doubleFoodInterval ) ) {
+            OPTIONS.doubleFoodInterval = options.doubleFoodInterval;
+        }
+
+        if ( Number.isInteger( options.snakeSpeed ) ) {
+            OPTIONS.snakeSpeed = options.snakeSpeed;
         }
     }
 }
 
 
+/**
+ * Save to local storage the current options values.
+ */
 export function save() {
     AppStorage.setData( { snake_options: OPTIONS } );
 }
 
 
-export function setDifficulty( difficulty: Difficulty ) {
-    OPTIONS.difficulty = difficulty;
+/**
+ * Get the current value of the given option.
+ */
+export function get<Key extends keyof OptionsData>( option: Key ) {
+    return OPTIONS[ option ];
 }
 
 
-export function getDifficulty() {
-    return OPTIONS.difficulty;
-}
-
-
-export function getDifficultyString() {
-    return Difficulty[ OPTIONS.difficulty ];
-}
-
-
-export function setColumns( columns: number ) {
-    OPTIONS.columns = columns;
-
-    changeCanvasDimensions( columns, OPTIONS.lines );
-}
-
-
-export function getColumns() {
-    return OPTIONS.columns;
-}
-
-
-export function setLines( lines: number ) {
-    OPTIONS.lines = lines;
-
-    changeCanvasDimensions( OPTIONS.columns, lines );
-}
-
-
-export function getLines() {
-    return OPTIONS.lines;
-}
-
-
-export function setFrame( yesNo: boolean ) {
-    OPTIONS.frameOn = yesNo;
-}
-
-
-export function getFrame() {
-    return OPTIONS.frameOn;
+/**
+ * Set a new value to the given option.
+ */
+export function set<Key extends keyof OptionsData>( option: Key, value: OptionsData[ Key ] ) {
+    OPTIONS[ option ] = value;
 }
